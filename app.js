3cdb9d2c2e499a87298ae7b0520025ebab0fcd5f
eraykalkan <eraykalkan6@gmail.com>
2022-01-08 23:41:04 +0300

    mvc yapısı ve crud işlemleri tamamlandı

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const path = require("path");
+const fileUpload = require("express-fileupload");
+const methodOverride = require("method-override");
 const ejs = require("ejs");
-const Photo = require("./models/Photo");
+const photoController = require('./controllers/photoControllers');
+const pageController = require('./controllers/pageController');
 
 const app = express();
 
@@ -10,6 +12,7 @@ const app = express();
 mongoose.connect("mongodb://localhost/pcat-test-db", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  useFindAndModify: false,
 });
 
 //TEMPLATE ENGINE olarak ejs kullanacağız
@@ -19,35 +22,26 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-app.get("/", async (req, res) => {
-  const photos = await Photo.find({});
-  res.render("index", {
-    photos,
-  });
-});
-
-//photo detay sayfası
-app.get("/photos/:id", async (req, res) => {
-  //res.render("about");
-  const photo = await Photo.findById(req.params.id)
-  res.render('photo', {
-    photo
+app.use(fileUpload());
+app.use(
+  methodOverride("_method", {
+    methods: ["POST", "GET"],
   })
-});
+);
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+app.get("/", photoController.getAllPhotos);
+app.get("/photos/:id", photoController.getPhoto);
+app.post("/photos", photoController.createPhoto);
+app.put("/photos/:id", photoController.updatePhoto);
+app.delete("/photos/:id", photoController.deletePhoto);
 
-app.get("/add", (req, res) => {
-  res.render("add");
-});
+app.get("/about", pageController.getAboutPage);
+app.get("/add", pageController.getAddPage);
+app.get("/photos/edit/:id", pageController.getEditPage);
 
-app.post("/photos", async (req, res) => {
-  await Photo.create(req.body);
-  res.redirect("/");
-});
+
+
+
 
 const port = 3000;
 app.listen(port, () => {
