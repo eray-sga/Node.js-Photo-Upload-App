22fe857d26e244a615ffa1f208f211b3c76cf880
eraykalkan <eraykalkan6@gmail.com>
2022-01-08 21:14:05 +0300

    fotoğraf ekleme, listeleme yapıldı

--- app.js
+++ app.js
@@ -1,14 +1,46 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const ejs = require("ejs");
+const Photo = require("./models/Photo")
 
 const app = express();
 
-//middleware static dosyalarımız için
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+//connect db
+mongoose.connect("mongodb://localhost/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 });
+
+
+//TEMPLATE ENGINE olarak ejs kullanacağız
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static("public"));
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({})
+  res.render("index", {
+    photos
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body)
+  res.redirect('/')
+});
+
 
 const port = 3000;
 app.listen(port, () => {
